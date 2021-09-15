08153432c152dd61758d2cfca95b1c8544bd471d
Kerem Unce <Keremunce@hotmail.com>
2021-09-16 00:49:36 +0300

    second comment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(window).scroll(function(){
     $('.navbar').toggleClass('scrolled', $(this).scrollTop()>50);
 });
+
+$('.owl-carousel').owlCarousel({
+
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:[  '<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+    navClass: ['owl-prev', 'owl-next'],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2.5
+        }
+    }
+})
